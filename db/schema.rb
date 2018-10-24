c868d26f009112dd653ca7fa6d8c331237086702
Jose Rodriguez <jmrv002@gmail.com>
2018-10-23 22:23:27 -0400

    add comments model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_021440) do
+ActiveRecord::Schema.define(version: 2018_10_24_022119) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "product_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
