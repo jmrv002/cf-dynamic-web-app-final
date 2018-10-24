e4493d965b7a9133da9dad2a70f24d86399bea28
Jose Rodriguez <jmrv002@gmail.com>
2018-10-23 22:18:32 -0400

    add  products and category models and migrations

---
+++ db/migrate/20181024021440_create_categories.rb
@@ -0,0 +1,14 @@
+class CreateCategories < ActiveRecord::Migration[5.2]
+  def change
+    create_table :categories do |t|
+      t.string :name
+
+      t.timestamps
+    end
+
+    create_table :products_categories, id: false do |t|
+      t.belongs_to :product, index: true
+      t.belongs_to :category, index: true
+    end
+  end
+end
