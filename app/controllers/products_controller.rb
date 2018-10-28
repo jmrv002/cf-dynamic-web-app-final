fa63e72890e9ecc4cf9e61747a9e01fe860ccf36
Jose Rodriguez <jmrv002@gmail.com>
2018-10-28 07:52:24 -0400

    add the permit values in product params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -76,6 +76,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.fetch(:product, {})
+      params.require(:product).permit(:name, :description, :image, :price, category_ids: [])
     end
 end
