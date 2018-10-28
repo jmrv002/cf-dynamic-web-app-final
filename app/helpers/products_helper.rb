ee5e5fa3c8b85863267d72fb690462784e527531
Jose Rodriguez <jmrv002@gmail.com>
2018-10-28 08:37:14 -0400

    add helpers for product form view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,14 @@
 module ProductsHelper
+  def get_label_for_product_form(product, name, text = '') 
+    active = 'active' unless product[name].nil?
+    text = name.capitalize if text == ''
+
+    "<label for='product_#{name}' class='#{active}'>#{text}</label>".html_safe
+  end
+
+  def get_category_option_for_product_form(product, category)
+    selected = 'selected' if product.categories.pluck(:id).include?(category.id)
+    
+    "<option value='#{category.id}' #{selected} >#{category.name}</option>".html_safe
+  end
 end
