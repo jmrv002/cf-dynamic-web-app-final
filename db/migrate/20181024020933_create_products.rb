63628eb4761fdf122ba1e6aeb1d4af2fc61721f4
Jose Rodriguez <jmrv002@gmail.com>
2018-10-29 23:31:09 -0400

    modify migrations

--- db/migrate/20181024020933_create_products.rb
+++ db/migrate/20181024020933_create_products.rb
@@ -5,7 +5,6 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.text :description
       t.string :image
       t.decimal :price
-      t.integer :rating, inclusion: 1..5
 
       t.timestamps
     end
