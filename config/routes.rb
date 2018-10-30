5cd6b67f2150e940206ac792e8a179056c0a603b
Jose Rodriguez <jmrv002@gmail.com>
2018-10-29 23:36:23 -0400

    remove filter

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-
   root 'products#index'
-
-  get 'products/filter', to: 'products#filter'
-
+  
   post 'comments', to: 'comments#create'
 
   resources :products
