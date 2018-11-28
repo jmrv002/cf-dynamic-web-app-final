a9c77191fb8b5a1b18e8470df1a8072bd3c8bc11
Jose Rodriguez <jmrv002@gmail.com>
2018-11-28 07:00:40 -0400

    change product img in the seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 productDescription = "Lorem ipsum dolor amet selfies truffaut butcher pour-over marfa, roof party vice. Ethical neutra gluten-free kale chips activated charcoal seitan XOXO cardigan wolf selfies YOLO. Intelligentsia lyft celiac, locavore prism freegan pour-over waistcoat ethical sartorial. Typewriter raclette coloring book quinoa. Air plant sriracha activated charcoal vegan irony coloring book. Stumptown +1 iceland celiac, four loko XOXO edison bulb salvia kombucha sartorial. Tbh hammock bespoke typewriter fanny pack banjo ugh williamsburg health goth pok pok pickled direct trade subway tile. Intelligentsia vaporware wolf flannel. Brunch small batch neutra blue bottle, mumblecore listicle glossier."
 
 p1 = Product.create(name: "Mac Desktop", description: productDescription, image_url: "https://i.imgur.com/HHBIYn5.png",colour: 'white' ,price: 800.00)
-p2 = Product.create(name: "Drone", description: productDescription, image_url: "https://i.imgur.com/L0tyRBB.png",colour: 'white' ,price: 400.00)
-p3 = Product.create(name: "Camara Lumix", description: productDescription, image_url: "https://imgur.com/rXT8tnB.png",colour: 'white' ,price: 200.00)
-p4 = Product.create(name: "PS4", description: productDescription, image_url: "https://imgur.com/iOQ4yjY.png",colour: 'white' ,price: 250.00)
+p2 = Product.create(name: "Drone", description: productDescription, image_url: "https://imgur.com/qxIuSTf.png",colour: 'white' ,price: 400.00)
+p3 = Product.create(name: "Camara Lumix", description: productDescription, image_url: "https://imgur.com/1N4xFAu.png",colour: 'white' ,price: 200.00)
+p4 = Product.create(name: "PS4", description: productDescription, image_url: "https://imgur.com/qcWcdsc.png",colour: 'white' ,price: 250.00)
 p5 = Product.create(name: "Mac Book", description: productDescription, image_url: "https://imgur.com/OBoGpkZ.png",colour: 'white' ,price: 500.00)
 
 o1 = Order.create(user_id: 1, product_id: p1.id, total: 10.0)
